e138efb521991332941259b319b74a86c186ac2c
remi.brenaut <remi.brenaut@epita.fr>
2025-03-01 20:48:13 +0100

    style: format all files with .prettierrc config

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.scss';
-import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import './App.scss'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
 import Portfolio from './components/Portfolio'
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
@@ -25,4 +25,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,14 @@ import { Link, NavLink } from 'react-router-dom'
 import LogoS from '../../assets/images/logo-r.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faSuitcase, faUser, faBars, faClose } from '@fortawesome/free-solid-svg-icons'
+import {
+  faEnvelope,
+  faHome,
+  faSuitcase,
+  faUser,
+  faBars,
+  faClose,
+} from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { useState } from 'react'
 
@@ -45,11 +52,11 @@ const Sidebar = () => {
           <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
         <FontAwesomeIcon
-        onClick={() => setShowNav(false)}
+          onClick={() => setShowNav(false)}
           icon={faClose}
           color="#ffd700"
           size="3x"
-          className='close-icon'
+          className="close-icon"
         />
       </nav>
       <ul>
